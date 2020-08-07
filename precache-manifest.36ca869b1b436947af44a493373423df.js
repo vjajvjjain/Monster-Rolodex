23c05353ffb2d6c501270af358141d9ed9d5e49b
vjajvjjain <vj98965@gmail.com>
2020-08-07 19:35:29 +0530

    Updates

---
+++ precache-manifest.36ca869b1b436947af44a493373423df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3f964387c5f785b296cae7cc8c7d42",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "d233aab9b170a6847344",
+    "url": "/Monster-Rolodex/static/css/main.6b558872.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d233aab9b170a6847344",
+    "url": "/Monster-Rolodex/static/js/main.60cf7a44.chunk.js"
+  },
+  {
+    "revision": "2b55c592b5ec82aad6ec",
+    "url": "/Monster-Rolodex/static/js/runtime-main.28f6930b.js"
+  }
+]);
